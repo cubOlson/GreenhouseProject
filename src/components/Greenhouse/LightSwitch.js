ec5217e83b4c650dc0bca4b70e399d8025e082da
mike <mikemihalchik@gmail.com>
2021-03-18 13:55:46 -0400

    climate context set up

--- src/components/Greenhouse/LightSwitch.js
+++ src/components/Greenhouse/LightSwitch.js
@@ -1,8 +1,13 @@
 import './LightSwitch.css';
+import {useTheme} from '../../context/ThemeContext.js'
 
 function LightSwitch() {
+  const { themeName, setThemeName} = useTheme();
+  const currTheme = themeName === 'day' ? 'day' : 'night';
+
+
   return (
-    <div className="light-switch day">
+    <div className={`light-switch ${currTheme}`}>
       <div className="on">DAY</div>
       <div className="off">NIGHT</div>
     </div>
--- src/context/ClimateContext.js
+++ src/context/ClimateContext.js
@@ -1,2 +1,21 @@
 // Temperature has a default value of 50 degrees
 // Humidity has a default value of 40%
+import { createContext, useContext, useState} from 'react';
+
+export const ClimateContext = createContext();
+
+export const useClimate = () => useContext(ClimateContext);
+
+export default function ClimateProvider(props) {
+    const [climateName, setClimateName] = useState(50);
+
+    return (
+        <ClimateContext.Provider
+        value={{
+            climateName,
+            setClimateName
+        }}>
+            {props.children}
+        </ClimateContext.Provider>
+    )
+}
